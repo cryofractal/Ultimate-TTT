2d7eebd0cefbeabb8a0452a83f7df8f9822c0a42
Cryofractal <160640838+cryofractal@users.noreply.github.com>
2026-01-26 23:36:52 -0500

    add rank n board generator test

---
+++ src/test.rs
@@ -0,0 +1,24 @@
+use crate::{cell::*, coord};
+
+fn generate_rank_n(n: u8) -> Cell {
+    let mut cell = Cell::new(n);
+    if n == 0 {
+        return cell;
+    }
+    let small_cell = generate_rank_n(n - 1);
+    let coord_list = vec![
+        coord![0, 0],
+        coord![0, 1],
+        coord![0, 2],
+        coord![1, 0],
+        coord![1, 1],
+        coord![1, 2],
+        coord![2, 0],
+        coord![2, 1],
+        coord![2, 2],
+    ];
+    for coord in coord_list {
+        cell.children.insert(coord, small_cell.clone());
+    }
+    cell
+}
